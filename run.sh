e514ea81a77b8975552273b62f979224296379ae
amilcamilo <65173256+amilcamilo@users.noreply.github.com>
2021-03-25 19:07:01 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=loggle_stock_market
+#SBATCH --cpus-per-task=30
+#SBATCH --mem-per-cpu=10G
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=amilcar.camilo@upf.edu
+
+module load R/4.0.2
+Rscript covidstonks.r
